3b98c20fc2d99665e2acdbf766d1ae7a280f076a
Sebastian Sameghini <s.sameghini@gmail.com>
2021-03-10 14:28:40 -0300

    Test first examples

--- server.js
+++ server.js
@@ -10,21 +10,44 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+// enable Body-Parser for URL decoding
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/views'));
+
+// For testing purposes
+app.use(function(req, res) {
+  var method = req.method;
+  var route = req.path;
+  var ip = req.ip;
+  console.log(`${method} ${route} - ${ip}`);
+});
+
+app.get('/json', function(req, res) {
+  var response = "Hello json";
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({message: response.toUpperCase()});
+  } else {
+    res.json({message: "Hello json"});
+  }
+});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile('/views/index.html');
 });
-
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+// Timestamp GET response
+app.get("/api/timestamp/", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
