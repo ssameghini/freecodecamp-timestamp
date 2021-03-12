071676f3fde892979e1348b2e9b97293b14c5a5f
Sebastian Sameghini <s.sameghini@gmail.com>
2021-03-12 19:03:52 -0300

    App done. POST request included

--- server.js
+++ server.js
@@ -10,19 +10,74 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// enable Body-Parser for URL decoding
+// Require and enable Body Parser
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static(__dirname + '/views'));
 
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile('/views/index.html');
+});
+
+// Timestamp GET response
+app.get("/api/timestamp/", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  if (/\d{5,}/.test(dateString)) {
+    const dateInt = parseInt(dateString);
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    let dateObject = new Date(dateString);
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
+});
+
+app.post('/api/timestamp',function(req, res) {
+  console.log(req.body.date);
+  var inputArray = req.body.date;
+  var dateString = inputArray.filter(input => input ? true : false);
+
+  if (/\d{5,}/.test(dateString)) {
+    const dateInt = parseInt(dateString);
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    let dateObject = new Date(dateString);
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
 // For testing purposes
-app.use(function(req, res) {
+app.use(function(req, res, next) {
   var method = req.method;
   var route = req.path;
   var ip = req.ip;
   console.log(`${method} ${route} - ${ip}`);
+  next();
+});
+
+// your first API endpoints... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
 
 app.get('/json', function(req, res) {
@@ -32,24 +87,4 @@ app.get('/json', function(req, res) {
   } else {
     res.json({message: "Hello json"});
   }
-});
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile('/views/index.html');
-});
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-// Timestamp GET response
-app.get("/api/timestamp/", (req, res) => {
-  res.json({ unix: Date.now(), utc: Date() });
-});
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
 });
